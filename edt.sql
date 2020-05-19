7ff012cab02a123c7c7bcf0e065d210793b12ad7
DESKTOP-1V7I5C7\Vivien <vivien.detournay@edu.ece.fr>
2020-05-19 14:31:08 +0200

    debut
    bdd
    main
    gitignore

---
+++ edt.sql
@@ -0,0 +1,196 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mar. 19 mai 2020 à 12:29
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `edt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cours`
+--
+
+DROP TABLE IF EXISTS `cours`;
+CREATE TABLE IF NOT EXISTS `cours` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NOM` text NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `enseignant`
+--
+
+DROP TABLE IF EXISTS `enseignant`;
+CREATE TABLE IF NOT EXISTS `enseignant` (
+  `ID_UTILISATEUR` int(11) NOT NULL,
+  `ID_COURS` int(11) NOT NULL,
+  PRIMARY KEY (`ID_UTILISATEUR`,`ID_COURS`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etudiant`
+--
+
+DROP TABLE IF EXISTS `etudiant`;
+CREATE TABLE IF NOT EXISTS `etudiant` (
+  `ID_UTILISATEUR` int(11) NOT NULL,
+  `NUMERO` text NOT NULL,
+  `ID_GROUPE` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `promotion`
+--
+
+DROP TABLE IF EXISTS `promotion`;
+CREATE TABLE IF NOT EXISTS `promotion` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NOM` text NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `salle`
+--
+
+DROP TABLE IF EXISTS `salle`;
+CREATE TABLE IF NOT EXISTS `salle` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NOM` text NOT NULL,
+  `CAPACITE` int(11) NOT NULL,
+  `ID_SITE` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `seance`
+--
+
+DROP TABLE IF EXISTS `seance`;
+CREATE TABLE IF NOT EXISTS `seance` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `SEMAINE` int(11) NOT NULL,
+  `DATE` date NOT NULL,
+  `HEURE_DEBUT` date NOT NULL,
+  `HEURE_FIN` date NOT NULL,
+  `ETAT` int(11) NOT NULL,
+  `ID_COURS` int(11) NOT NULL,
+  `ID_TYPE` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `seance_enseignants`
+--
+
+DROP TABLE IF EXISTS `seance_enseignants`;
+CREATE TABLE IF NOT EXISTS `seance_enseignants` (
+  `ID_SEANCE` int(11) NOT NULL,
+  `ID_ENSEIGNANT` int(11) NOT NULL,
+  PRIMARY KEY (`ID_SEANCE`,`ID_ENSEIGNANT`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `seance_groupes`
+--
+
+DROP TABLE IF EXISTS `seance_groupes`;
+CREATE TABLE IF NOT EXISTS `seance_groupes` (
+  `ID_SEANCE` int(11) NOT NULL,
+  `ID_GROUPE` int(11) NOT NULL,
+  PRIMARY KEY (`ID_SEANCE`,`ID_GROUPE`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `seance_salles`
+--
+
+DROP TABLE IF EXISTS `seance_salles`;
+CREATE TABLE IF NOT EXISTS `seance_salles` (
+  `ID_SEANCE` int(11) NOT NULL,
+  `ID_SALLE` int(11) NOT NULL,
+  PRIMARY KEY (`ID_SEANCE`,`ID_SALLE`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `site`
+--
+
+DROP TABLE IF EXISTS `site`;
+CREATE TABLE IF NOT EXISTS `site` (
+  `ID` int(11) NOT NULL,
+  `NOM` text NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `type_cours`
+--
+
+DROP TABLE IF EXISTS `type_cours`;
+CREATE TABLE IF NOT EXISTS `type_cours` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NOM` text NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `EMAIL` text NOT NULL,
+  `PASSWD` text NOT NULL,
+  `NOM` text NOT NULL,
+  `PRENOM` text NOT NULL,
+  `DROIT` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
