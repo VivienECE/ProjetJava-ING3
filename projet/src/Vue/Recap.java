44c68ab919800113edefd729f946f22a67f50de9
clemencejldm <clemencejldm@gmail.com>
2020-06-05 10:47:03 +0200

    recap

--- projet/src/Vue/Recap.java
+++ projet/src/Vue/Recap.java
@@ -72,14 +72,68 @@ public class Recap extends JFrame{
         
       
         ArrayList<Seance> Seances = controleur.getSeances();
-        ArrayList<Integer> lesIdCours = new ArrayList<Integer>();
+        //ArrayList<Integer> lesIdCours = new ArrayList<Integer>();
+        ArrayList<Seance> chaqueCours = new ArrayList<Seance>();
         LocalDate dO;
         LocalDate dF;
         int n;
+        int[] nb = {0,0,0,0,0,0,0,0,0,0};
         String nom;
         
-        //Voir combien de matiere differentes
-        int cours1 = Seances.get(0).getID_COURS();
+        //Trier par matiere
+        for(int k=1; k<=10; k++){
+            for(int i=0; i<Seances.size();i++){
+                if(Seances.get(i).getID_COURS()==k)
+                    chaqueCours.add(Seances.get(i));
+            }
+        }
+        
+        Object[][] data = new Object[10][4];    
+        
+        for(int x=1; x<=10; x++){
+            dO = LocalDate.of(2020, 07, 30);
+            dF = LocalDate.of(2020, 01, 01);
+            n=0;
+            nom = "";
+            for(int j=0; j<chaqueCours.size(); j++){
+                if(chaqueCours.get(j).getID_COURS()==x){//si c'est la meme matiere
+                    if((chaqueCours.get(j).getDATE()).isBefore(dO))
+                            dO = (chaqueCours.get(j).getDATE());
+                        if((chaqueCours.get(j).getDATE()).isAfter(dF))
+                            dF = (chaqueCours.get(j).getDATE());
+
+                    nom = chaqueCours.get(j).getCOURS(chaqueCours.get(j).getID_COURS());
+                    n++;//Une séance en plus
+                }
+            }
+            data[x-1][0]= nom;
+            data[x-1][1]= dO;
+            data[x-1][2]= dF;
+            data[x-1][3]= n;
+        }
+            //System.out.println(chaqueCours.get(j).getCOURS(chaqueCours.get(j).getID_COURS()));
+        
+        /*     nb[0]++;
+            if(Seances.get(i).getID_COURS()==2)//phy
+                nb[1]++;
+            if(Seances.get(i).getID_COURS()==3)//elec
+                nb[2]++;
+            if(Seances.get(i).getID_COURS()==4)//info
+                nb[2]++;
+            if(Seances.get(i).getID_COURS()==5)//anglais
+                nb[3]++;
+            if(Seances.get(i).getID_COURS()==6)//TDG
+                nb[4]++;
+            if(Seances.get(i).getID_COURS()==7)//Analyse
+                nb[5]++;
+            if(Seances.get(i).getID_COURS()==8)//PSTE
+                nb[6]++;
+            if(Seances.get(i).getID_COURS()==9)//Arabe
+                nb[7]++;
+            if(Seances.get(i).getID_COURS()==10)//DDT
+                nb[9]++;*/
+        
+        /*int cours1 = Seances.get(0).getID_COURS();
         lesIdCours.add(0,cours1);
         
         for(int i = 1; i < Seances.size(); i++)
@@ -91,14 +145,13 @@ public class Recap extends JFrame{
                 }
             }
         } 
+        System.out.println(Seances.size());
+        System.out.println(lesIdCours.size());
         
-        Object[][] data = new Object[lesIdCours.size()][4];
+        
         //Trier par matière
         for(int x=0; x<lesIdCours.size(); x++){
-            dO = LocalDate.of(2020, 07, 30);
-            dF = LocalDate.of(2020, 01, 01);
-            n=0;
-            nom = "";
+            
             for(int k=0; k<Seances.size(); k++){
                 if(Seances.get(k).getID_COURS()== lesIdCours.get(x)){
                     if((Seances.get(k).getDATE()).isBefore(dO))
@@ -110,12 +163,8 @@ public class Recap extends JFrame{
                     n++;//Une séance en plus
                 }
             }
-            data[x][0]= nom;
-            data[x][1]= dO;
-            data[x][2]= dF;
-            data[x][3]= n;
             
-        }
+        }*/
         return data;
     }
     
